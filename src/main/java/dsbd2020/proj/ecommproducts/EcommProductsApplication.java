e38f0058cece4a6eea139dffde6f0083104eee39
beatricegagliano <gaglianobea96@gmail.com>
2020-12-24 11:03:31 +0100

    first commit

---
+++ src/main/java/dsbd2020/proj/ecommproducts/EcommProductsApplication.java
@@ -0,0 +1,13 @@
+package dsbd2020.proj.ecommproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsbd2020/proj/ecommproducts/EcommProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package dsbd2020.proj.ecommproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
