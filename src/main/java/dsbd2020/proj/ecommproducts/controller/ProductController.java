9d5f5a7d3f92838ea6076de9b546c05db49575e4
CristinaGagliano <gaglianocristina2@gmail.com>
2020-12-26 12:13:30 +0100

    update products controller

--- src/main/java/dsbd2020/proj/ecommproducts/controller/ProductController.java
+++ src/main/java/dsbd2020/proj/ecommproducts/controller/ProductController.java
@@ -1,13 +1,12 @@
 package dsbd2020.proj.ecommproducts.controller;
 
-
-import dsbd2020.proj.ecommproducts.data.ProductsRepository;
 import dsbd2020.proj.ecommproducts.products.Products;
+import dsbd2020.proj.ecommproducts.service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+
 
 
 @Controller
@@ -15,29 +14,29 @@ import java.util.Optional;
 public class ProductController {
     
     @Autowired
-    ProductsRepository repository;
+    ProductsService service;
 
     @RequestMapping(value= "/products",method= RequestMethod.POST)
     public @ResponseBody
     Products addProducts (@RequestBody Products products){
-        return repository.save(products);
+        return service.addProducts(products);
     }
 
     @RequestMapping(value= "/products/{id}",method= RequestMethod.GET)
     public @ResponseBody
-    Optional<Products> getProducts(@PathVariable Integer id){
-        return repository.findById(id);
+    Products getProducts(@PathVariable Integer id){
+        return service.getProducts(id);
     }
 
     @RequestMapping(path= "/products/{id}",method= RequestMethod.PUT)
     public @ResponseBody
      Products updateProducts (@RequestBody Products products, @PathVariable Integer id){
-        return repository.save(products);
+        return service.updateProducts(products,id);
     }
 
     @RequestMapping(value= "/products",method= RequestMethod.GET)
     public @ResponseBody
     Iterable<Products> getProductsAll(){
-        return repository.findAll();
+        return service.getProductsAll();
     }
 }
